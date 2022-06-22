8cf9f206bd2c16c0c34e71379b1ba0e2b189115e
gwinplane <maxmarinin@gmail.com>
2022-06-22 09:58:50 +0300

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.iswebp();
+
+ // core version + navigation, pagination modules:
+ import Swiper, { Navigation, Pagination } from 'swiper';
+ const swiper = new Swiper();
